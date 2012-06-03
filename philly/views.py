06ebdcbf655a679bd23e61b8d5922cbc1a9c4a11
Adam Hinz <hinz.adam@gmail.com>
2012-06-03 00:08:49 -0400

    search working

--- philly/views.py
+++ philly/views.py
@@ -1,6 +1,7 @@
 import datetime
 import json
 
+from django.db.models import Q
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseServerError
@@ -80,15 +81,71 @@ def search_schools(request):
     missing = req["absences"]    # Max
     late = req["tardies"]        # Max
     behavior = req["discipline"] # 0=> None, 1 => Recent, 2 => 'Past'
-
+    
     pssa_math = req["pssa_math"] # 2=> Advanced 1=> Proficient, 3=> Near Proficient, 4=> Not Proficient, -1=> None
     pssa_reading = req["pssa_reading"] # 2=> Advanced 1=> Proficient, 3=> Near Proficient, 4=> Not Proficient, -1=> None
 
-    national_rank = req["national_rank"] #0=> Ignore, otherwise percentile
+    national_rank = int(req["national_rank"]) #0=> Ignore, otherwise percentile
 
     grades = req["grades"] #0=> As & Bs,  1=> 1 c in minor subj, 2=> 1 c in major subj, 3=> 2+ c's, 4=> Failed
 
-    return list_schools(request)
+    schools = School.objects.all()
+
+    schools = schools.filter(allowed_absences__gte=missing)
+    schools = schools.filter(allowed_tardy__gte=late)
+
+    if behavior == "1": # Got a recent dicipline, only neighborhood schools
+        schools = schools.filter(school_type=0) # Only neighborhood schools
+    elif behavior == "2":
+        schools = schools.filter(Q(behavior_time=1) | Q(behavior_time=-1))
+
+    if pssa_math == "1":
+        schools = schools.filter(Q(pssa_math=1)|Q(pssa_math=0)|Q(pssa_math=3))
+    elif pssa_math == "3":
+        schools = schools.filter(Q(pssa_math=0)|Q(pssa_math=3))
+    elif pssa_math == "4":
+        schools = schools.filter(school_type=0) # Only neighborhood schools
+
+    if pssa_reading == "1":
+        schools = schools.filter(Q(pssa_reading=1)|Q(pssa_reading=0)|Q(pssa_reading=3))
+    elif pssa_reading == "3":
+        schools = schools.filter(Q(pssa_reading=0)|Q(pssa_reading=3))
+    elif pssa_reading == "4":
+        schools = schools.filter(school_type=0) # Only neighborhood schools
+
+    if national_rank > 0:
+        schools = schools.filter(national_norm__lte=national_rank)
+
+    if grades == "0":
+        #TODO: But we do need to filter on time
+        pass # If As & Bs then we don't have to filter on anything
+    elif grades == "1":
+        # Filter where (# of c's allowed is >= 1 and minor is allowed) OR
+        #              (# of c's allowed is >= 0 and major is allowed)
+        schools = schools.filter(Q(grading_style_c_num__gte=1,grading_style_c=1) | Q(grading_style_c_num__gte=0,grading_style_c=2))
+    elif grades == "2":
+        # Filter where number of c's allowed is greater than or equal to 1 and C's are allowed in major
+        # subjects
+        schools = schools.filter(grading_style_c_num__gte=1,grading_style_c=2)
+    elif grades == "3": # 2+c's, so, 
+        # TODO: We are ignoring time in this
+        # TODO: We should probably split this into major/minor
+        schools = schools.filter(grading_style_c_num__gte=2)
+    elif grades == "4":
+        schools = schools.filter(school_type=0) # Only neighborhood schools
+
+    # Explicitly exclude neighborhood schools (this will interact with above 
+    # to filter to zero if we picked neighborhood only)
+
+    n = School.objects.filter(school_type=0).order_by('name')
+    c = schools.filter(school_type=1).order_by('name')
+    s = schools.filter(school_type=2).order_by('name')
+
+
+    return {"neighborhood": [school2dict(z) for z in n],
+            "citywide": [school2dict(z) for z in c],
+            "selective": [school2dict(z) for z in s] }
+
 
     
 
